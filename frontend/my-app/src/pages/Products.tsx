9647f7a88f11038abe10a37414a2b3f7a7ccb113
Sara.Escamilla <sara.escamilla@ENCEDCCUULT0722.local>
2025-08-25 21:51:08 -0600

    feat:add Products.ts main structure and DataTable component libraries

---
+++ frontend/my-app/src/pages/Products.tsx
@@ -0,0 +1,15 @@
+import DataTable from "react-data-table-component";
+import {useState,useEffect} from 'react'
+
+export default function Product(){
+    const [data,setData]=useState([])
+    const [loading,setLoading]=useState(false)
+    const [perPage,setPerPage]= useState (10)
+
+    return(
+        <div>
+            
+        </div>
+    )
+
+}
